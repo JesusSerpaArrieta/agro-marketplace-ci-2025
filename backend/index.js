06e75632db127becc7def218b74ed4911114a10d
JESUS Y DIEGO <jesus.serpa@cecar.edu.co>
2025-11-23 09:12:59 -0500

    feat(MC-001): registro de campesino con interfaz web funcional

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use(express.static('../frontend'));  // Sirve el frontend
 
 app.get('/', (req, res) => {
   res.send('API Agro Marketplace - CI funcionando');
